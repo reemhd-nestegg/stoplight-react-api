db8aa443735e270ab734954da0158deae8f44a80
Reem Hd <reem@nestegg.ai>
2023-12-22 08:49:29 +0000

    error due to false handles

--- src/login/LoginForm.tsx
+++ src/login/LoginForm.tsx
@@ -1,51 +1,69 @@
-import React, { useEffect } from 'react';
-import PartnerLoginForm from './PartnerLoginForm';
-import GeneralLoginForm from './GeneralLoginForm';
+import { useEffect, useState } from "react";
+import PartnerLoginForm from "./PartnerLoginForm";
+import GeneralLoginForm from "./GeneralLoginForm";
 
-
-export const LoginForm = ({ onApiChange } :any) => {
-  const [token, setToken] = React.useState('');
-
-  const [loginData, setLoginData] = React.useState(false);
-  const [showPartnerForm, setShowPartnerForm] = React.useState(false);
+export const LoginForm = ({ onApiChange }: any) => {
+  const [token, setToken] = useState("");
+  const [loginData, setLoginData] = useState(null); // Initialized to null instead of false
+  const [showPartnerForm, setShowPartnerForm] = useState(false);
 
   useEffect(() => {
-    fetch('https://testing.nestegg.ai/api/v1/authenticate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(loginData),
-    }).then((response) => response.json()).then((response) => {
-      if (!response.token) {
-        return;
-      }
-      
-      
-      setToken(response.token);
-    });
-  }, [loginData]);
-  
-  useEffect(() => {
-    if (!token) {
-      return;
+    if (loginData) {
+      fetch("http://localhost:3000/api/v1/authenticate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.token) {
+            setToken(response.token);
+          }
+        })
+        .catch((error) => {
+          console.error("Error during authentication:", error);
+        });
     }
-    
-    fetch('https://testing.nestegg.ai/api/v1/openapi/openapi.yml', {
-      method: 'GET',
-      headers: new Headers({
-        'content-type': 'application/json',
-        'x-access-token': token
-      }),
-    }).then((response) => response.text()).then((response) => {
-      onApiChange('data:text/plain;charset=UTF-8;base64,' + btoa(unescape(encodeURIComponent(response))))
-    });
-  }, [token]);
+  }, [loginData]);
+
+  useEffect(() => {
+    if (token) {
+      fetch("http://localhost:3000/api/v1/openapi/openapi.yml", {
+        method: "GET",
+        headers: new Headers({
+          "content-type": "application/json",
+          "x-access-token": token,
+        }),
+      })
+        .then((response) => response.text())
+        .then((response) => {
+          onApiChange(
+            "data:text/plain;charset=UTF-8;base64," +
+              btoa(unescape(encodeURIComponent(response)))
+          );
+        })
+        .catch((error) => {
+          console.error("Error fetching OpenAPI spec:", error);
+        });
+    }
+  }, [token, onApiChange]);
+
+  const togglePartnerForm = () => {
+    setShowPartnerForm((prevShowPartnerForm) => !prevShowPartnerForm);
+  };
 
   return (
-    <div className='login-form'>
-        {showPartnerForm ? <PartnerLoginForm onFormSubmit={setLoginData} /> : <GeneralLoginForm onFormSubmit={setLoginData} />}
-        <a href="#" onClick={() => setShowPartnerForm(!showPartnerForm)}>{showPartnerForm ? 'General Login' : 'Partner Login'}</a>
+    <div className="login-form">
+      {showPartnerForm ? (
+        <PartnerLoginForm onFormSubmit={setLoginData} />
+      ) : (
+        <GeneralLoginForm onFormSubmit={setLoginData} />
+      )}
+      <a href="#!" onClick={togglePartnerForm}>
+        {showPartnerForm ? "General Login" : "Partner Login"}
+      </a>
     </div>
   );
 };
